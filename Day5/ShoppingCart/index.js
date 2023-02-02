d42c3c7cd74f86aa6edddcee6dc057985e64cc98
sijampala <100946155+sijampala@users.noreply.github.com>
2023-02-02 11:46:35 +0530

    Commit_IndexFile

--- Day5/ShoppingCart/index.js
+++ Day5/ShoppingCart/index.js
@@ -2,6 +2,10 @@ const express=require("express");//module to be installed
 const path=require("path");
 const bodyParser=require("body-parser");//module to be installed
 
+const morgan=require("morgan");//module to be installed
+// logging of the requests -- morgan
+
+
 const fs=require("fs");
 
 const port=3000;
@@ -12,10 +16,11 @@ var empArr = [{ empId: 101, empName: "Asha", salary: 1001, deptId: "D1" },
 { empId: 105, empName: "Keshav", salary: 3500, deptId: "D2" },
 { empId: 106, empName: "Pran", salary: 4000, deptId: "D3" },
 { empId: 107, empName: "Saurav", salary: 3800, deptId: "D3" }]
-var wStream=fs.createWriteStream(path.join(__dirname,"log","serverLog.txt"),);
+var wStream=fs.createWriteStream(path.join(__dirname,"log","serverLog.txt"),{flags:"a"});
 
 
 var app=express();
+app.use(morgan("combined"));
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
@@ -26,6 +31,9 @@ app.use((request,response,next)=>{
     wStream.write(`Request Method : ${request.method}; Request url:${request.url}; Date: ${now}`);
     next();
 })
+
+app.use(express.static(path.join(__dirname,"public","files")));
+app.use(express.static(path.join(__dirname,"public","images")));
 
 app.use((request,response,next)=>{
     console.log("Inside the first custom middleware");
@@ -39,10 +47,31 @@ app.use((request,response,next)=>{
         next();
     }
 })
+app.use((request,response,next)=>{
+    console.log("New middleware");
+    next();
+})
+
+app.use((request,response,next)=>{
+    request.userName="asha";
+    next();
+})
+
+app.use((request,response,next)=>{
+    setTimeout(()=>{
+        request.password="asha";
+    },5000)
+    next();
+})
+app.use((request,response,next)=>{
+    console.log("Password",request.password);// asha or ud
+  
+    next();
+})
 
 app.post("/employees",(request,response)=>{
-  console.log(request);
-  response.end("Post request received")
+  empArr.push(request.body);
+  response.end("Employee Details added successfully")
 })
 
 app.get("/employees",(request,response)=>{
@@ -52,6 +81,20 @@ app.get("/employees",(request,response)=>{
     response.send(empArr);
 })
 
+app.get("/employee",(request,response)=>{
+    var pos=empArr.findIndex(item =>item.empName == request.userName);
+    if(pos >=0)
+    {
+        var str1="Employee Details"+ JSON.stringify(empArr[pos]);
+        response.send(str1);
+    }
+    else
+    {
+        response.sendStatus(404);
+        response.send("Employee not found")
+    }
+    response.send(empArr);
+})
 app.get("/login",(request,response)=>{
     var filePath=path.join(__dirname,"public","login.html");
     response.sendFile(filePath);
